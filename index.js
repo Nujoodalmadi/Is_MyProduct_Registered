a8c9bd18ca7632ffa994dea7354097257e76d3ac
Nujood AlMadhi <nujoodalmadi@gmail.com>
2019-11-30 20:47:17 +0300

    search functionality and product cards added

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-$.ajax({
-  url:
-    "https://data.gov.sa/Data/ar/dataset/7ef8bcb5-62d7-4dce-bdb3-95aa06032f8a/resource/04ddc748-7051-49e1-af16-168fdc8798f0/download/list-of-registered-oral-hygiene-products.json",
-  success: function(data) {
-    var filteredData = $.grep(data, function(product, i) {
-      return (
-        product.BrandName === "ORAL B" ||
-        product.ProductEnName === "Kin Care Mouthwash 250 ml"
+$("button").click(function() {
+  //Emptying previous search
+  $("#products").empty();
+  $.ajax({
+    url:
+      "https://data.gov.sa/Data/ar/dataset/7ef8bcb5-62d7-4dce-bdb3-95aa06032f8a/resource/04ddc748-7051-49e1-af16-168fdc8798f0/download/list-of-registered-oral-hygiene-products.json",
+    success: function(data) {
+      var search = $("input").val();
+      //Filtering the array of products
+      var filteredData = $.grep(data, function(product, i) {
+        return searchFunction(product, search);
+      });
+      filteredData.forEach(product =>
+        $("#products").append(`
+        <div id="product" class="m-sm-auto col-sm-2 col-12 col-sm-10 col-md-8 col-lg-7 col-xl-3 m-auto pt-5 pt-xl-0">
+          <div class="fdb-box fdb-touch">
+            <h4>${product.ProductEnName}</h4>
+            <h4>${product.ProductArName}</h4>
+            <h4>Brand: ${product.BrandName}</h4>
+            <p>Listed Name: ${product.ProductArName}</p>
+            <p>Counrty: ${product.CounrtyEn}</p>
+            <p>Valume: ${product.PackageVolume}</p>
+            <p>Manufacturer: ${product.manufactureenglishname}</p>
+            <p># ${product.ProductNumber}</p> 
+            <p class="mt-4"><a href="https://www.froala.com">Learn More <i class="fas fa-angle-right"></i></a></p>
+          </div>
+        </div>
+   `)
       );
-    });
-
-    console.log(filteredData);
-  },
-  error: function(e) {
-    console.log(e);
-  }
+    },
+    error: function(e) {
+      console.log(e);
+    }
+  });
 });
+
+// Returning the filtering condition
+searchFunction = function(product, search) {
+  return (
+    product.BrandName.toString()
+      .toLowerCase()
+      .includes(search.toLowerCase()) ||
+    product.ProductArName.toString().includes(search) ||
+    product.ProductEnName.toString()
+      .toLowerCase()
+      .includes(search.toLowerCase())
+  );
+};
