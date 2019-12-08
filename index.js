4993c5b4a1c032b0a8e664df57dece07c0a8109e
Nujood AlMadhi <nujoodalmadi@gmail.com>
2019-12-08 21:47:03 +0300

    loading and design changes

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-$("#oral, #nails, #hair, #skin").click(function() {
-  //get data url from the clicked button's data-url attribute
-  var url = $(this).attr("data-url");
+$(document).ready(function() {
+  buttonSwitch();
 
-  $("#products").html(
-    '<img id="loader-img" alt="" src="https://miro.medium.com/max/882/1*9EBHIOzhE1XfMYoKz1JcsQ.gif" width="100" height="100" align="center" />'
-  );
+  $("#oral, #nails, #hair, #skin").click(function() {
+    //get data url from the clicked button's data-url attribute
+    var url = $(this).attr("data-url");
 
-  $.ajax({
-    type: "GET",
-    cache: true,
-    url: url,
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader(
-        "X-CKAN-API-Key",
-        "f358fc15-8983-4a77-a5f7-ada7d1069a4e"
-      );
-    },
-    success: function(data) {
-      var search = $("input").val();
-      $("input").val("");
-      //Filtering the array of products
-      var filteredData = $.grep(data, function(product, i) {
-        return searchFunction(product, search);
-      });
-      //empty previous search and remove loading
-      $("#products").empty();
+    //append loading icon, which will be removed after ajax we get ajax response
+    $("button").append('<i class="fa fa-circle-o-notch fa-spin"></i>');
 
-      if (filteredData.length === 0) {
-        $("#products").append(`<h1>nothing</h1>`);
-      }
+    //Ajax call with url obtained from button attribute
+    $.ajax({
+      type: "GET",
+      cache: true,
+      url: url,
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader(
+          "X-CKAN-API-Key",
+          "f358fc15-8983-4a77-a5f7-ada7d1069a4e"
+        );
+      },
+      success: function(data) {
+        var search = $("input").val();
+        $("input").val("");
 
-      filteredData.forEach(product =>
-        $("#products").append(`
+        //Filtering the array of products based on search condition
+        var filteredData = $.grep(data, function(product, i) {
+          return searchFunction(product, search);
+        });
+
+        //empty previous search and remove loading
+        $("#products").empty();
+
+        //show message "nothing found" if search result == 0
+        if (filteredData.length === 0) {
+          $("#products").append(`<h1>nothing</h1>`);
+        }
+
+        //if search result != 0, show hidden HTML section and append the products in that section
+        $("#productsSection").attr("hidden", false);
+        filteredData.forEach(product =>
+          $("#products").append(`
 
         <div id="product" class="paged-element m-sm-auto col-sm-2 col-12 col-sm-10 col-md-8 col-lg-7 col-xl-3 m-auto pt-5 pt-xl-0">
           <div class="fdb-box fdb-touch">
@@ -48,28 +56,43 @@ $("#oral, #nails, #hair, #skin").click(function() {
           </div>
         </div>
    `)
-      );
-    },
-    error: function(e) {
-      $("#products").html(`<h1>Sorry, something went wrong</h1>`);
-      console.log(e);
-    }
+        );
+        //remove loading icon
+        $("i").remove();
+        buttonSwitch();
+      },
+      error: function(e) {
+        $("#products").html(`<h1>Sorry, something went wrong</h1>`);
+        console.log(e);
+        $("i").remove();
+        buttonSwitch();
+      }
+    });
   });
-});
 
-// Returning the filtering condition
-searchFunction = function(product, search) {
-  return (
-    product.BrandName.toString()
-      .toLowerCase()
-      .includes(search.toLowerCase()) ||
-    product.ProductArName.toString().includes(search) ||
-    product.ProductEnName.toString()
-      .toLowerCase()
-      .includes(search.toLowerCase())
-  );
-};
+  // Returning the filtering condition
+  searchFunction = function(product, search) {
+    return (
+      product.BrandName.toString()
+        .toLowerCase()
+        .includes(search.toLowerCase()) ||
+      product.ProductArName.toString().includes(search) ||
+      product.ProductEnName.toString()
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    );
+  };
 
-$("#report").click(function() {
-  $("input").val("");
+  // Disable all buttons if the search box is empty
+  function buttonSwitch() {
+    $("button").prop("disabled", true);
+    $("#searchBox").keyup(function() {
+      $("button").prop("disabled", this.value == "" ? true : false);
+    });
+  }
+
+  //empty report input box after submitting the form
+  $("#report").click(function() {
+    $("#reportInput").val("");
+  });
 });
